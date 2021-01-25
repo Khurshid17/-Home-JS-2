e0886cd1047c2a17e8658c31aa8455f10dd735a5
khurshid <rakhmatullaevkhurshid17@gmail.com>
2021-01-25 20:28:56 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,150 @@
+/* var age = +prompt('Введите свой возрост');
+
+if (age >= 0 && age <= 18) {
+    alert('Вы еще молоды. Вам нужно учится');
+} else if (age >= 18 && age <= 50) {
+    alert('Вам нужно работать');
+} else if (age >= 50 && age <=59) {
+    alert('Вам скоро на пенсию');
+} else if (age >= 59 && age <=150) {
+    alert('Вы пенсионер');
+}
+
+else {
+    alert('Ошибка');
+} */
+
+// var time = +prompt('Введите время от 0 до 23');
+
+/* if (time == 0) {
+    alert('12 часов ночи');
+} else if (time == 1) {
+    alert('час ночи');
+} else if (time == 2) {
+    alert('2 часа ночи');
+} else if (time == 3) {
+    alert('3 часов утра');
+} else if (time == 4) {
+    alert('4 часов утра');
+} else if (time == 5) {
+    alert('5 часов утра');
+} else if (time == 6) {
+    alert('6 часов утра');
+} else if (time == 7) {
+    alert('7 часов утра');
+} else if (time == 8) {
+    alert('8 часов утра');
+} else if (time == 9) {
+    alert('9 часов утра');
+} else if (time == 10) {
+    alert('10 часов дня');
+} else if (time == 11) {
+    alert('11 часов дня');
+} else if (time == 12) {
+    alert('12 часов дня');
+} else if (time == 13) {
+    alert('час дня');
+} else if (time == 14) {
+    alert('2 часов дня');
+} else if (time == 15) {
+    alert('3 часов дня');
+} else if (time == 16) {
+    alert('4 часов дня');
+} else if (time == 17) {
+    alert('5 часов дня');
+} else if (time == 18) {
+    alert('6 часов вечера');
+} else if (time == 19) {
+    alert('7 часов вечера');
+} else if (time == 20) {
+    alert('8 часов вечера');
+} else if (time == 21) {
+    alert('9 часов вечера');
+} else if (time == 22) {
+    alert('10 часов вечера');
+} else if (time == 23) {
+    alert('11 часов вечера');
+}
+else {
+    alert('Ошибка');
+} */
+
+
+/* switch (time) {
+    case 0:
+        alert('12 часов ночи');
+        break;
+    case 1:
+        alert('час ночи');
+        break;
+    case 2:
+        alert('2 часа ночи');
+        break;
+    case 3:
+        alert('3 часов утра');
+        break;
+    case 4:
+        alert('4 часов утра');
+        break;
+    case 5:
+        alert('5 часов утра');
+        break;
+    case 6:
+        alert('6 часов утра');
+        break;
+    case 7:
+        alert('7 часов утра');
+        break;
+    case 8:
+        alert('8 часов утра');
+        break;
+    case 9:
+        alert('9 часов утра');
+        break;
+    case 10:
+        alert('10 часов дня');
+        break;
+    case 11:
+        alert('11 часов дня');
+        break;
+    case 12:
+        alert('12 часов дня');
+        break;
+    case 13:
+        alert('час дня');
+        break;
+    case 14:
+        alert('2 часов дня');
+        break;
+    case 15:
+        alert('3 часов дня');
+        break;
+    case 16:
+        alert('4 часов дня');
+        break;
+    case 17:
+        alert('5 часов дня');
+        break;
+    case 18:
+        alert('6 часов вечера');
+        break;
+    case 19:
+        alert('7 часов вечера');
+        break;
+    case 20:
+        alert('8 часов вечера');
+        break;
+    case 21:
+        alert('9 часов вечера');
+        break;
+    case 22:
+        alert('10 часов вечера');
+        break;
+    case 23:
+        alert('11 часов вечера');
+        break;
+
+    default:
+        alert('Ошибка');
+        break;
+} */
